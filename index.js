9d11c3d764341809b5716e9aa96c737463c6ad9c
wagner <wagner.santos@accurate.com.br>
2022-02-23 10:43:41 -0300

    Usando regex para extrair links do arquivo

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ function tratarErro(erro){
     throw new Error(chalk.red(erro.code, 'Nenhum arquivo ou diretório encontrado.'))
 }
 
+function extrairLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm  //Global e Multilinha
+    const arrayResultados = []
+    let temp;
+    while ((temp = regex.exec(texto)) !== null){
+        arrayResultados.push({[temp[1]] : temp[2]})
+    }
+
+    return arrayResultados
+}
+
+
 // Exemplo async/await
 async function pegarArquivo(caminhoDoArquivo){
     encoding = 'utf-8'
     try{
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto))
+        console.log(extrairLinks(texto))
     }catch(erro){
         tratarErro(erro)
     }finally{
