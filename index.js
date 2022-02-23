5cd0f5bcfbfe2ba37c0967a66e3ab7cfd5f1d81b
wagner <wagner.santos@accurate.com.br>
2022-02-22 22:27:29 -0300

    Pequeno exemplo de async await

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ function tratarErro(erro){
     throw new Error(chalk.red(erro.code, 'Nenhum arquivo ou diretório encontrado.'))
 }
 
-function pegarArquivo(caminhoDoArquivo){
+// Exemplo async/await
+async function pegarArquivo(caminhoDoArquivo){
     encoding = 'utf-8'
-    fs.promises
-    .readFile(caminhoDoArquivo, encoding)
-    .then((texto) => chalk.green(console.log(texto)))
-    .catch((erro) => tratarErro(erro))
+    try{
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto))
+    }catch(erro){
+        tratarErro(erro)
+    }
+    
 }
 
+// Exemplo de Promises (mais antigo)
+// function pegarArquivo(caminhoDoArquivo){
+//     encoding = 'utf-8'
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => chalk.green(console.log(texto)))
+//     .catch((erro) => tratarErro(erro))
+// }
+
+// Exemplo sincrono
 // function pegarArquivo(caminhoDoArquivo){
 //     const encoding = 'utf-8'
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
