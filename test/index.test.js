c4403ad8d4c3b67bc1e28bc60f716c5205d8af5e
wagner <wagner.santos@accurate.com.br>
2022-02-25 15:35:29 -0300

    Incluido testes iniciais com Jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,27 @@
 const pegarArquivo = require('../index')
 
-test('it must be a function', () => {
-    expect(typeof pegarArquivo).toBe('function')
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pegarArquivo::', () => {
+    it('it must be a function', () => {
+        expect(typeof pegarArquivo).toBe('function')
+    })
+    it('it must return array "resultados"', async () => {
+        const resultado = await pegarArquivo('./test/arquivos/texto1.md')
+        expect(resultado).toEqual(arrayResult)
+    })
+    it('it must return "Não há links a serem tratados"', async () => {
+        const resultado = await pegarArquivo('./test/arquivos/texto_semlink.md')
+        expect(resultado).toBe("Não há links a serem tratados")
+    })
+    it('it must return "Nenhum arquivo ou diretório encontrado."', () => {
+        async function capturarErro(){
+            await pegarArquivo('./test/arquivos')
+            exepct(capturarErro).toThrowError(/Nenhum arquivo ou diretório encontrado./)
+        }       
+    })
 })
