25db6766c1637fcdc6d8026f1fa2df17cf1a137c
wagner <wagner.santos@accurate.com.br>
2023-03-08 11:33:05 -0300

    Adicionada estrutura para validacao dos links

--- cli.js
+++ cli.js
@@ -1,18 +1,30 @@
 import pegaArquivo from "./aula_006_regex.js";
 import chalk from "chalk";
 import fs from 'fs';
+import listaValidada from "./http_validacao.js";
+
 
 const caminho = process.argv;
 
-function imprimeResultado(resultado, identificador=''){
-    console.log(
-        chalk.blueBright("Lista de Links:"), 
-        chalk.yellow.bgBlack(identificador),
-        resultado)
+function imprimeResultado(valida, resultado, identificador=''){
+    if(valida){
+        console.log(
+            chalk.blueBright("Lista Validada:"), 
+            chalk.yellow.bgBlack(identificador),
+            listaValidada(resultado)
+        )
+    } else{
+        console.log(
+            chalk.blueBright("Lista de Links:"), 
+            chalk.yellow.bgBlack(identificador),
+            resultado
+        )
+    }
 }
 
 async function processaArquivo(argumentos){
     const caminho = argumentos[2]
+    const valida = argumentos[3] === '--valida'
 
     try{
         fs.lstatSync(caminho)
@@ -25,12 +37,12 @@ async function processaArquivo(argumentos){
 
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(caminho);
-        imprimeResultado(resultado)
+        imprimeResultado(valida, resultado)
     } else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDoArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDoArquivo}`)
-            imprimeResultado(lista, nomeDoArquivo)
+            imprimeResultado(valida, lista, nomeDoArquivo)
         })
     }
 }
