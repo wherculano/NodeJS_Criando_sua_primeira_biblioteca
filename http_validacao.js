eef63d0b5d20b825a6b68d571a68acec12ec7db8
wagner <wagner.santos@accurate.com.br>
2023-03-10 01:01:16 -0300

    Adicionado tratamento de erros para links invalidos

--- http_validacao.js
+++ http_validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks(arrayLinks){
     return arrayLinks.map((objetoLinks) => Object.values(objetoLinks).join())
 }
@@ -5,15 +7,29 @@ function extraiLinks(arrayLinks){
 async function checaStatus(listaURLs){
     const arrayStatus = await Promise.all(
         listaURLs.map(async (url) => {
-            const response = await fetch(url)
-            return response.status
+            try{
+                const response = await fetch(url)
+                return `${response.status} - ${response.statusText}`
+            } catch(erro){
+                return manejaErros(erro)
+            }
         })
     )
     return arrayStatus;
 }
 
+function manejaErros(erro){
+    if(erro.cause.code === 'ENOTFOUND'){
+        return "Link não encontrado!"
+    }else{
+        return "Ocorreu algum erro!"
+    }
+}
 export default async function listaValidada(listaDeLinks){
     const lista = extraiLinks(listaDeLinks)
     const status = await checaStatus(lista)
-    return status
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status_code: status[indice]
+    }))
 }
